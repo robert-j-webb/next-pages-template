ec468989dbade3da1f86d0eec8d68661fdad309f
Robert Webb <robertwebb@modular.com>
2024-03-12 10:41:21 -0700

    Stil fails without the optimize package imports line

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,6 @@ export default withBundleAnalyzer({
     ignoreDuringBuilds: true,
   },
   experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/modals'],
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
 });
